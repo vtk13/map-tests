74702b4497ee87c548ee8967b922db1ccf378149
Rogov Viktor <vtk666@gmail.com>
2015-09-12 17:03:01 +0600

    Fix erlang benchmark

--- erlang/bench.erl
+++ erlang/bench.erl
@@ -6,23 +6,23 @@
 start() ->
   mnesia:create_schema([node()]),
   mnesia:start(),
-  mnesia:create_table(map, [{attributes, record_info(fields, map)}]),
+  mnesia:create_table(map, [{disc_copies, [node()]}, {attributes, record_info(fields, map)}]),
   ok.
 
 bench(N) ->
   timer:tc(?MODULE, insert_items, [N]).
 
-insert_item(0) ->
+insert_item(0, _) ->
   ok;
-insert_item(N) ->
-  Item = #map{id=N, x=N, y=N},
+insert_item(N, Base) ->
+  Item = #map{id=N + Base, x=N, y=N},
   mnesia:write(Item),
-  insert_item(N-1).
+  insert_item(N-1, Base).
 
 insert_items(0) ->
   ok;
 insert_items(N) ->
   mnesia:transaction(fun() ->
-      insert_item(100)
+      insert_item(100, N * 100)
   end),
   insert_items(N-1).
